742a37a5b4d1d11096f151f2c4b75c6fff15a036
jockerwi <wanz.khazura@gmail.com>
2024-09-30 12:44:14 +0700

    Conditional Statement

---
+++ belajar-if-else-Conditional-Statement.js
@@ -0,0 +1,43 @@
+//&& bisa d artikan dan
+//&& semua kebutuhan harus terpenuhi
+let nilai = 1;
+
+if(nilai >= 80 && nilai <= 100 ) {nilai = "A"    
+}else if(nilai >= 70 && nilai < 80 ) {nilai = "V"    
+}else if(nilai >= 60 && nilai < 70 ) {nilai = "C"
+}else {nilai = "D"
+}
+console.log(nilai)
+
+//or (||) bisa di pakai saat salah satu kondisi terpenuhi lalu dia akan menghasilakan nilai true
+nilai = 50;
+let banding = nilai >= 80 && nilai <= 100; //kenapa false karna tidak terpenuhi jadi hasilnya dan &&
+console.log(banding, "&&")
+banding2 = nilai >= 80 || nilai <= 100; //kenapa hasil nya jadi true karna nilai nya 50 kurang dari 100 jadi true
+console.log(banding2, "||")
+//! artinya tidak jika bernilai sama dengan ! jadi false
+let banding3 = nilai != 50; 
+console.log(banding3, "!" )
+
+//apabedannya == dan ===
+let x = 50; //number
+let y = "50"; //string
+let banding4 = x == y;
+console.log(banding4, "4")
+let banding5 = x === y;
+console.log(banding4, "4")
+
+let number =3;
+if (number>5) {
+    if (number % 2 === 0){
+        console.log("lebih besar dari 5 dan genap")
+    } else {
+        console.log("lebih besar dari 5 dan ganjil")
+    }
+}else {
+    if (number % 2 === 0){
+        console.log("lebih kecil dari 5 dan genap")
+    } else {
+        console.log("lebih kecil dari 5 dan ganjil")
+    }
+}
--- belajar-if-else.js
+++ belajar-if-else.js
@@ -1,6 +1,8 @@
+//Belajar if else contoh =
+
 let nilai = 20;
 
-// tergantung si nilainya jadi let hasil nilai yang di proses di console.log nya itu penilainya 
+// tergantung si nilainya jadi let hasil nilai dan if yang di proses di console.log nya itu penilainya dan else beberapa nilai dari nilainya
 
 if(nilai >= 50) { //true or false
     console.log ("lulus GOOD JOB")
