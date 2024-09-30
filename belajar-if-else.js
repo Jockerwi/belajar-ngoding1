8daff710567944f151d1e7d9c9a963a87fdd24a2
jockerwi <wanz.khazura@gmail.com>
2024-09-30 09:28:11 +0700

    Belajar if else eps1

---
+++ belajar-if-else.js
@@ -0,0 +1,13 @@
+let nilai = 20;
+
+// tergantung si nilainya jadi let hasil nilai yang di proses di console.log nya itu penilainya 
+
+if(nilai >= 50) { //true or false
+    console.log ("lulus GOOD JOB")
+} else if (nilai >= 40) {
+    console.log("Maaf anda tidak lulus")
+} else if (nilai >= 30) {
+    console.log("Maaf anda bodoh")
+} else {
+    console.log("Mati aja kau")
+}
